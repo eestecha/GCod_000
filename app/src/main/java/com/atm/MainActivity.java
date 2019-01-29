a79c5fcc7775e802478ac4ca658acdb8bffd4e8c
lioemi <emilio.estecha@gmail.com>
2019-01-29 22:57:00 +0100

    Metido AR antiguo de atm.

--- app/src/main/java/com/atm/MainActivity.java
+++ app/src/main/java/com/atm/MainActivity.java
@@ -13,14 +13,19 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.atm.ar.AugmentedReality;
 import com.atm.db.PS_countries_DAO;
 import com.atm.db._DBHelper;
 import com.atm.util.K;
+
+import org.openintents.intents.WikitudePOI;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
 	private ListAdapter mLstAdpt;
 	private ListView mLv_main;
+
+	private AugmentedReality mAugmentedReality;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,6 +97,9 @@ public class MainActivity extends AppCompatActivity {
                 .setOnClickListener(
                         new View.OnClickListener() {
                             public void onClick(View v) {
+
+                            	ar();
+
                                 /*Toast.makeText(mCtx, "Button LOAD PS Clicked", Toast.LENGTH_LONG).show();*/
 
                                 // Tip: If you need to package a file in your app that is accessible at install time,
@@ -174,6 +184,53 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 		mLv_main.setAdapter(mLstAdpt);
+
+	}
+
+	private void ar() {
+
+		List<WikitudePOI> wikiPOIs = new ArrayList<WikitudePOI>();
+		WikitudePOI poi = null;
+		double dLat = 0.0;
+		double dLon = 0.0;
+		/////////////////////////////////////////////////////////////
+    	// Tonterias:
+    	dLat = 40.447992; dLon = -3.577701;
+    	poi = new WikitudePOI(
+    			dLat,	// Lat
+    			dLon,	// Lon
+    			600.0,	// Altitud (?)
+    			"Goye", // Nom.Cliente
+    			"Despacho de Javier Goyeneche"   // Direcc.
+    	);
+    	poi.setIconresource( this.getResources().getResourceName( R.drawable.pin_azul ));
+    	wikiPOIs.add( poi );
+
+    	dLat = 40.448049; dLon = -3.577679;
+    	poi = new WikitudePOI(
+    			dLat,	// Lat
+    			dLon,	// Lon
+    			600.0,	// Altitud (?)
+    			"Unai", // Nom.Cliente
+    			"Despacho de Unai García"   // Direcc.
+    	);
+    	// poi.setIconresource( this.getResources().getResourceName( R.drawable.pin_azul ));
+    	wikiPOIs.add( poi );
+
+    	dLat = 40.449095; dLon = -3.576778;
+    	poi = new WikitudePOI(
+    			dLat,	// Lat
+    			dLon,	// Lon
+    			600.0,	// Altitud (?)
+    			"Hotel", // Nom.Cliente
+    			"El hotel de ahí."   // Direcc.
+    	);
+    	// poi.setIconresource( this.getResources().getResourceName( R.drawable.pin_azul ));
+    	wikiPOIs.add( poi );
+
+
+		mAugmentedReality = new AugmentedReality( this );
+		mAugmentedReality.launchARView( wikiPOIs, R.drawable.pin_rojo );
 
 	}
 
